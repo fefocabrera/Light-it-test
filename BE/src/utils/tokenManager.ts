1eb2d689694ed0be1b5054402e6b21110fe308a8
Federico <fefocabrera@gmail.com>
2023-06-16 17:39:23 -0300

    jwt secret key from .env

--- BE/src/utils/tokenManager.ts
+++ BE/src/utils/tokenManager.ts
@@ -1,7 +1,13 @@
 import jwt from "jsonwebtoken";
+import { GeneralError } from '../exceptions/exceptions';
+
+const secretKey = process.env.JWT_SECRET_KEY;
 
 export const getToken = (userUuid: string): string => {
-    const token = jwt.sign({userUuid}, "keyTesttttt");
-
-    return token;
+    if (secretKey) {
+        const token = jwt.sign({userUuid}, secretKey);
+        return token;
+    } else {
+        throw new GeneralError(`Missing JWT Private key.`);
+    }
 }
